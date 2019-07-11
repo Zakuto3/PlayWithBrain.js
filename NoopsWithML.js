9ec949c095461a8113406a32a2899b6e0a3717bc
Zakuto3 <Zakuto3@gmail.com>
2019-07-11 16:57:39 +0200

    Collect data and init network added

--- NoopsWithML.js
+++ NoopsWithML.js
@@ -1,25 +1,57 @@
 const brain = require('brain.js');
 const fetch = require('node-fetch');
 const fs = require('fs');
-const network = new brain.NeuralNetwork({activation:'leaky-relu'});
+let trainData = require('./Data.json');
 let url = 'https://api.noopschallenge.com';
 
-network.train([
-    {input:[1], output:[1]},
-    {input:[0], output:[0]}
-])
+const network = new brain.recurrent.LSTM();
 
-const output = network.run([1]);
+let testData = trainData[trainData.length - 1];
+trainData.pop();
 
-//main
-(async ()=>{
-    let data = await fetchPost1("/riddlebot/start",{login:"zakuto3"});
-    data = await fetchPost1(data.riddlePath);
-    console.log("BEFORE: ",data.riddleText);
-    data = reverseString(data.riddleText);
-    console.log("AFTER: ",data);
-    fs.writeFile("H:/TESSST/BrainLab/Data.json", "Hello",()=>{console.log("Write to File Done to ", __dirname);});
-})();
+let slicedData = trainData.slice(0,1);
+
+network.train([{input:"hell", output:"lleh"}]);
+const output = network.run('eh'); 
+
+console.log("INPUT: ", testData.input);
+console.log("OUTPUT: ", testData.output);
+console.log("AI-Chan: ", output);
+
+function saveFile(){
+    fs.writeFile("network.json", JSON.stringify(network.toJSON()), function(err) {
+        if(err)
+            return console.log(err);
+
+        console.log("The file was saved!");
+        //loadFile()
+    });
+}
+
+function loadFile(){
+    return obj = JSON.parse(fs.readFileSync('network.json', 'utf8'));
+}
+
+//main(); 
+async function main(){
+    let storedData = [];
+    let outputData;
+    let data;
+
+    data = await fetchPost1("/riddlebot/start",{login:"zakuto3"});
+    for (let index = 0; index < 100; index++) {
+
+        
+        data = await fetchPost1(data.riddlePath);
+        //console.log("BEFORE: ",data.riddleText);
+        outputData = reverseString(data.riddleText);
+        //console.log("AFTER: ",data);
+        storedData.push({input:data.riddleText,output:outputData});
+        
+    }
+
+    fs.writeFile("H:/TESSST/BrainLab/Data.json",JSON.stringify(storedData),()=>{console.log("Write to File Done to ", __dirname);});
+}
 
 
 function reverseString(str) {
